bdcb6a3141de4523ed0a9dee179859440919a607
nvcompsci <31483891+nvcompsci@users.noreply.github.com>
2021-10-13 13:54:11 -0400

    added fake data

--- src/Store.js
+++ src/Store.js
@@ -2,7 +2,18 @@ import React, { useState } from 'react'
 
 function Store() {
     const [items,setItems] = useState([
-        {}
+        {
+            id: 1,
+            name: "Banana (bunch)",
+            price: 0.89,
+            inventory: 7
+        },
+        {
+            id: 2,
+            name: "Banana (bunch)",
+            price: 0.89,
+            inventory: 7
+        },
     ])
 
     return (
